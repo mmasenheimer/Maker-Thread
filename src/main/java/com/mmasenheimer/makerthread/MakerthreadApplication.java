31eac0bff55b11ba6e8363be274ee82bad9120a8
mmasenheimer <mmasenheimer21@gmail.com>
2025-06-25 23:37:04 -0700

    Initial commit

---
+++ src/main/java/com/mmasenheimer/makerthread/MakerthreadApplication.java
@@ -0,0 +1,13 @@
+package com.mmasenheimer.makerthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerthreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakerthreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmasenheimer/makerthread/MakerthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmasenheimer.makerthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerthreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
